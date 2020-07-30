392f58fe81af636c139327b7e9eeac238c2a8729
Theo <theo.depresle@gmail.com>
2020-07-31 00:41:03 +0200

    adding note and copyright

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Page from "./components/Page";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function App() {
     return (
         <div className="App">
             <Page/>
+
+            <div className={'notes'}>
+                <p><a href={"#"} onClick={window.print}>Imprimer</a> cette page pour avoir un obtenir mon CV au format PDF.</p>
+
+                <p>Build with React and <FontAwesomeIcon icon={faHeart} className={'red'}/></p>
+
+                <p>&copy; 2020 Theo Depresle</p>
+            </div>
         </div>
     );
 }
