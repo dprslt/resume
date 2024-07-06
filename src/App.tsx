533bde96c9776d0decd08e1033dd4185e8fb5455
dprslt <theo.depresle@gmail.com>
2024-07-07 00:21:42 +0200

    Add tily logo and fix css bu in print media

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,48 @@ import "./setupI18n.ts";
 import { useTranslation } from "react-i18next";
 
 
+import tily from "./static/tily.png";
+
 const App: FC<React.PropsWithChildren<unknown>> = () => {
-  const { t } = useTranslation(); 
+  const { t } = useTranslation();
 
   return (
     <div className="App">
+
+      <div className="app-header">
+        <div className="tily">
+          <img src={tily} alt={"Logo Tily"} className={'tily-logo'} height={50} />
+          <h2>TILY</h2>
+        </div>
+
         <div className={"int"}>
           <LangFlag locale={"fr"} flagLocale={"fr"} />
           <LangFlag locale={"en"} flagLocale={"us"} />
         </div>
+      </div>
 
-        <Page />
+      <Page />
 
-        <div className={"notes"}>
-          <p>
-            <a
-              href={"/#"}
-              onClick={(e) => {
-                e.preventDefault();
-                window.print();
-              }}
-            >
-              {t('print')}
-            </a>
-            {' '}
-            {t('print_this_page')}
-          </p>
-          <p>
-            Build with React, Sass and{" "}
-            <FontAwesomeIcon icon={faHeart} className={"red"} />
-          </p>
-          <p>&copy; {new Date().getFullYear()} Theo Depresle</p>
-        </div>
+      <div className={"notes"}>
+        <p>
+          <a
+            href={"/#"}
+            onClick={(e) => {
+              e.preventDefault();
+              window.print();
+            }}
+          >
+            {t('print')}
+          </a>
+          {' '}
+          {t('print_this_page')}
+        </p>
+        <p>
+          Build with React, Sass and{" "}
+          <FontAwesomeIcon icon={faHeart} className={"red"} />
+        </p>
+        <p>&copy; {new Date().getFullYear()} Theo Depresle</p>
+      </div>
     </div>
   );
 };
